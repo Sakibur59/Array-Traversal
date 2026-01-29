8f1a05d3ac0615911d64114278d8e9bc4df06a57
Sakibur59 <sakiburrahman5978@gmail.com>
2026-01-29 19:36:48 +0600

    Using Array for of loop

---
+++ problem1.js
@@ -0,0 +1,11 @@
+let arr = [24, 53, 2532, 6, 74, 47, 74, 84, 2, 20];
+
+for (const element of arr) {
+  console.log(element);
+}
+
+let arr1 = ["fardin", "jannat", "sara", "rafa", "ayat"];
+
+for (const element1 of arr1) {
+  console.log(element1);
+}
