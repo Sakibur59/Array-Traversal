7f70d424bbabe4bc01fecadd8aae88734e65c091
Sakibur59 <sakiburrahman5978@gmail.com>
2026-01-29 20:15:40 +0600

    Array reversed

---
+++ problem5.js
@@ -0,0 +1,11 @@
+const statement = "I am a hard working person";
+
+const arr = statement.split(" ").reverse().join(" ");
+
+console.log(arr);
+
+
+let arr2 = ["fardin", "jannat", "sara", "rafa", "ayat"];
+
+arr2.reverse();
+console.log(arr2)
