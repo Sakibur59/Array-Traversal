e18f7dfe91ed26572a4608a8716dc7597b860132
Sakibur59 <sakiburrahman5978@gmail.com>
2026-01-31 21:18:38 +0600

    function boolean

--- funtion.js
+++ funtion.js
@@ -1,24 +1,36 @@
-function doThis(){
-    console.log('solve this math');
-    console.log('complete Homework');
-    console.log("complete your meal");
+function doThis() {
+  console.log("solve this math");
+  console.log("complete Homework");
+  console.log("complete your meal");
 }
-doThis()
+doThis();
 
-
-//using return 
-function add(num1,num2){
-    const result = num1+num2;
-    return result
+//using return
+function add(num1, num2) {
+  const result = num1 + num2;
+  return result;
 }
 
-const result = add(20,1);
-console.log(result)
+const result = add(20, 1);
+console.log(result);
 
-function square(number){
-
-    let total = number*number
-    console.log("square the given parameter: ",total);
+function square(number) {
+  let total = number * number;
+  console.log("square the given parameter: ", total);
 }
 
 square(10);
+
+function double(number, value) {
+    console.log(number,value)
+    if(value === true){
+        let result = number *4;
+        return result;
+    } else {
+        let result = number *2;
+        return result;
+    }
+}
+
+console.log(double(40,true));
+console.log(double(20,false));
