a36fa9a5ce2267390f87eed75809ae0420c0fbeb
Sakibur59 <sakiburrahman5978@gmail.com>
2026-01-31 21:22:48 +0600

    function array length

--- funtion.js
+++ funtion.js
@@ -34,3 +34,13 @@ function double(number, value) {
 
 console.log(double(40,true));
 console.log(double(20,false));
+
+
+function elements(number){
+    const len = number.length;
+    return len;
+}
+
+console.log("Array length: ",elements([12,31,4,51,7,51,54,13,45]))
+
+// let arrLen=
