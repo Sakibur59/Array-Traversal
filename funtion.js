4f1c73d56b9db58aa60bd00c955a28eb10757df8
Sakibur59 <sakiburrahman5978@gmail.com>
2026-01-31 21:04:28 +0600

    function return

--- funtion.js
+++ funtion.js
@@ -4,3 +4,14 @@ function doThis(){
     console.log("complete your meal");
 }
 doThis()
+
+
+//using return 
+function add(num1,num2){
+    const result = num1+num2;
+    return result
+}
+
+const result = add(20,1);
+console.log(result)
+
